bf4598ae75db3b60bac649cd21f2c14bdd81d0d4
OrtegaMichaela <ortegamichaela6@gmail.com>
2023-03-09 21:31:55 -0200

    My Educational Background Added

--- git-python-ortega.py
+++ git-python-ortega.py
@@ -1,3 +1,7 @@
 print("Michaela G. Ortega")
 print("Tanza Boac Marinduque")
 print("September 18, 2003")
+
+print("Bachelor of Science in Information Technology")
+print("Second Year")
+print("Section A")
